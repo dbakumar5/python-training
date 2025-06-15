addd24af945dc53e762f7d9b3e72dfb0523c927a
dbakumar5 <dbakumar5@gmail.com>
2025-06-15 03:22:15 +0100

    test script for calculating days based on given hours
    test script for calculating days based on given hours

---
+++ Test.py
@@ -0,0 +1,11 @@
+hours = int(input("Enter number of hours:\n"))
+if hours >= 0:
+    if hours >= 24:
+        def cal_hours(str1,hours, str2):
+         days= hours/24
+         print(str1,hours,str2, days)
+        cal_hours("Number of days for", hours, "hours is" )
+    else:
+       print("Enter value above 24")
+else:
+    print("please provide value greater than 0")
